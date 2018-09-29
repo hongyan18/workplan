3e294ffa6cbbb8855a9beb34eb61f2b343f76cf7
hongyan18 <595892312@qq.com>
2018-09-29 18:04:24 +0800

    新增日历，子组件向父组件传值（实现动态传值）

--- src/demo/Weekplanset.vue
+++ src/demo/Weekplanset.vue
@@ -5,22 +5,88 @@
          <div class="input_content">
              <textarea placeholder="请输入本周计划内容"></textarea>
         </div>
+        <div class="starttime">
+            <van-icon name="clock" style="float: left; left: 13%;line-height: 40px;"   color="black"  size="20px" />
+            <div class="title">开始时间：<span style="color:#33a8ff;" @click="showStartPopup">{{startTime}}</span></div>
+        </div>
+        <div class="endtime">
+            <van-icon name="clock" style="float: left; left: 13%;line-height: 40px;"   color="black"  size="20px" />
+            <div class="title">结束时间：<span style="color:#33a8ff;" @click="showEndtPopup">{{endtime}}</span></div>
+        </div>
+       <!-- 开始时间 -->
+        <van-popup v-model="starttimePopup" position="bottom" :overlay="false">
+        <div style="overflow: hidden;">
+             <div style="float:left;margin-left: 16px" @click="coloetime">取消</div>
+             <div style="float:right;margin-right: 16px"  @click="coloetime">确定</div>
+        </div>
+        <seedtime ref="child" v-on:returnparent="seedAlterParents"></seedtime>
+        </van-popup>
+        <!-- 结束时间s -->
+         <van-popup v-model="endtimePopup" position="bottom" :overlay="false">
+        <div style="overflow: hidden;">
+             <div style="float:left;margin-left: 16px" @click="coloeendtime">取消</div>
+             <div style="float:right;margin-right: 16px"  @click="coloeendtime">确定</div>
+        </div>
+        <seedtime ref="child" v-on:returnparent="seedAlterParente"></seedtime>
+        </van-popup>
+
         <div class="save">
             <button>保存</button>
         </div>
     </div>
 </template>
 <script>
+import seedtime from './time.vue'
 export default{
+  components: { seedtime },
   data () {
     return {
+
+      currentDate: new Date(),
+      minDate: new Date(2013, 12, 1),
       title: '新增本周计划',
-      contenttype: false
+      contenttype: false,
+      startTime: '2018-09-17',
+      endtime: '2018-10-17',
+      starttimePopup: false,
+      endtimePopup: false
+
     }
   },
   methods: {
+    // 接收子组件 time传来的值 改变开始时间 startTime
+    seedAlterParents: function (data) {
+      this.startTime = data
+    },
+    // 接收子组件 time传来的值 改变借宿时间 endTime
+    seedAlterParente: function (data) {
+      this.endtime = data
+    },
+    // 关闭开始时间子组件
+    coloetime: function () {
+      this.starttimePopup = false
+    },
+    coloeendtime: function () {
+      this.endtimePopup = false
+    },
     goback: function () {
       window.history.back()
+    },
+    /* 弹出开始日期选择控件 */
+    showStartPopup: function () {
+      if (this.starttimePopup === false) {
+        this.starttimePopup = true
+      } else {
+        this.starttimePopup = false
+      }
+    },
+    // 弹出结束日期选择控件
+    showEndtPopup: function () {
+      if (this.endtimePopup === false) {
+        this.endtimePopup = true
+      } else {
+        this.endtimePopup = false
+      }
     }
   }
 }
@@ -28,12 +94,12 @@ export default{
 <style scoped>
 .input_content{
     width: 100%;
-    height: 400px;
+    height: 227px;
 }
 
 .input_content textarea{
     width: 96%;
-    height: 350px;
+    height: 227px;
     padding: 0 2%;
     border:none;
     overflow: auto;
@@ -56,5 +122,21 @@ export default{
     border-radius: 8px;
     border: 1px solid #33a8ff;
     color: white;
+}
+.starttime{
+    height: 40px;
+    width: 100%;
+    background-color: #fff;
+    margin-top: 50px;
+}
+.title{
+    line-height: 40px;
+    float: left;
+    margin-left: 14%;
+}
+.endtime{
+    height: 40px;
+    width: 100%;
+    background-color: #fff;
 }
 </style>
--- src/demo/time.vue
+++ src/demo/time.vue
@@ -1,58 +1,205 @@
 <template>
-    <div>
-        <div>
-            <p>年份</p>
-            <p>月份</p>
+    <div class="calendar-container">
+        <div class="year">
+            <div>
+                <span class="fl" @click="lastYear">«</span>
+                <span class="fl" @click="lastMonth">‹</span>
+                <p>{{nowDate.year}}年{{nowDate.month+1}}月</p>
+                <span class="fr" @click="nextYear">»</span>
+                <span class="fr" @click="nextMonth">›</span>
+            </div>
         </div>
-        <ul class="weekdays">
-            <li>一</li>
-            <li>二</li>
-            <li>三</li>
-            <li>四</li>
-            <li>五</li>
-            <li>六</li>
-            <li>日</li>
+        <ul class="week">
+           <li v-for="(o,index) in 7" :key="o">{{formatWeek(index)}}</li>
         </ul>
-        <button @click="test">test</button>
+        <ul class="date">
+            <li class="none-week" v-for="o in lastMonthDays" :key="o+50" >{{lastMonthStartDay+o-1}}</li>
+            <li class="datelicli" @click="clickEvent" v-for="day in nowMonthDays" :key="day" >{{day}}</li>
+            <li class="none-week" v-for="day in (42-lastMonthDays-nowMonthDays)" :key="day+100">{{day}}</li>
+        </ul>
     </div>
 </template>
+
 <script>
-export default{
+export default {
+  props: ['start-date'],
   data () {
     return {
-      //   res: '1', // 0:1是否闰年
-      nstr: '', // 当前Date咨询
-      ynow: '', // 年份
-      mnow: '', // 月份
-      n1str: '', // 当月第一天Date资讯
-      firstday: '', // 当月第一天星期几
-      m_days: '' // 各月份总天数
+      selectDate: [], // 选择日期列表
+      nowDate: this.getDate(new Date()), // 当前设置时间 默认为当前系统时间
+      testtime: ''
+    }
+  },
+  computed: {
+    lastMonthDays () {
+      return this.startWeek()
+    },
+    lastMonthStartDay () {
+      return this.calcLastMonthDays(this.nowDate.year, this.nowDate.month) - (this.startWeek() - 1)
+    },
+    nowMonthDays () {
+      return this.calcDays(this.nowDate.year, this.nowDate.month)
     }
   },
   created () {
-
+    if (this.setDate) {
+      this.nowDate = this.getDate(this.setDate)
+    }
   },
   methods: {
 
-    is_leap: function (year) {
-      // 判断是否为闰年
-      return (year % 100 === 0 ? (year % 400 === 0 ? 1 : 0) === 1 : (year % 4 === 0 ? 1 : 0) === 1)
+    getDate (date) {
+      return {
+        year: date.getFullYear(),
+        month: date.getMonth(),
+        day: date.getDay(),
+        date: date.getDate()
+      }
     },
-    test: function () {
-      this.nstr = new Date()
-      this.ynow = this.nstr.getFullYear() // 年份
-      this.mnow = this.nstr.getMonth() // 月份
-      this.n1str = new Date(this.ynow, this.mnow, 1) // 当月第一天Date资讯
-      this.firstday = this.n1str.getDay() // 当月第一天星期几
-      this.m_days = [31, 28 + this.$options.methods.is_leap(2004), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-      alert(this.m_days)
+    formatWeek (day) {
+      switch (day) {
+        case 0:
+          return '日'
+        case 1:
+          return '一'
+        case 2:
+          return '二'
+        case 3:
+          return '三'
+        case 4:
+          return '四'
+        case 5:
+          return '五'
+        case 6:
+          return '六'
+      }
+    },
+    // 判断闰年
+    isLeapYear (year) {
+      return (year % 4 === 0) && (year % 100 !== 0 || year % 400 === 0)
+    },
+    // 根据日子计算星期
+    calcWeekend (year, month, day) {
+      return new Date(year, month, day).getDay()
+    },
+    // 计算某年某月的天数
+    calcDays (year, month) {
+      const monthDay = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+      if (this.isLeapYear(year) && month === 1) { return 29 } else { return monthDay[month] }
+    },
+    // 计算上个月天数
+    calcLastMonthDays (year, month) {
+      if (month === 0) {
+        return this.calcDays(year - 1, 11)
+      } else {
+        return this.calcDays(year, month - 1)
+      }
+    },
+    // 上月
+    lastMonth () {
+      if (this.nowDate.month === 0) {
+        this.nowDate.month = 11
+        this.nowDate.year--
+      } else {
+        this.nowDate.month--
+      }
+    },
+    // 下月
+    nextMonth () {
+      if (this.nowDate.month === 11) {
+        this.nowDate.month = 0
+        this.nowDate.year++
+      } else {
+        this.nowDate.month++
+      }
+    },
+    // 去年
+    lastYear () {
+      this.nowDate.year--
+    },
+    // 下一年
+    nextYear () {
+      this.nowDate.year++
+    },
+    // 计算当月开始星期
+    startWeek () {
+      return this.calcWeekend(this.nowDate.year, this.nowDate.month, 1)
+    },
+    //
+    clickEvent (e) {
+      let monthNo = this.nowDate.month
+      let month = monthNo <= 11 ? (monthNo + 1) : 0
+      let date = {
+        year: this.nowDate.year,
+        month: month,
+        week: new Date(this.nowDate.year, this.nowDate.month, e.target.innerText).getDay(),
+        day: Number(e.target.innerText)
+      }
+      let timevalue = date.year + '-' + date.month + '-' + date.day
+      this.testtime = timevalue
+      // 将最后鼠标点击的时间传回父组件
+      this.$emit('returnparent', timevalue)
+      console.log(timevalue)
+      this.$emit('click-event', date)
+      return this.testtime
     }
   }
 }
 </script>
-<style scoped>
- .weekdays li{
-    float: left;
-    width: 13%;
- }
+
+<style>
+    .calendar-container {
+        padding: 2vw 3vw;
+    }
+    .year {
+        text-align: center;
+        margin-bottom: 10px;
+        height: 30px;
+    }
+    .week,.date{
+        box-sizing: border-box;
+        display: flex;
+        flex-wrap: wrap;
+        list-style: none;
+    }
+    .week {
+        border-bottom: .5px solid #ddd;
+        margin-bottom: 5px;
+    }
+    .week li,.date li {
+        font-size: 14px;
+        width: calc(94vw/7);
+        height: 30px;
+        text-align: center;
+        line-height: 30px;
+    }
+
+    .datelicli:hover{
+      /* background-color:  rgb(51, 168, 255); */
+     color:rgb(51, 168, 255);
+    }
+    .none-week {
+        color: #aaa;
+    }
+    .year>div {
+        height: 30px;
+        overflow: hidden;
+    }
+    .year span {
+        line-height: 30px;
+        font-size: 20px;
+        display:  inline-block;
+        width: 10%;
+    }
+    .year p {
+        line-height: 1;
+        width: 50%;
+        display: inline-block;
+    }
+    .fl {
+        float: left;
+    }
+    .fr {
+        float: right;
+    }
 </style>
